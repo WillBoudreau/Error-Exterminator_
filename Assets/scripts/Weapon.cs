821e2e5e83690d14538d605918567ee9de76dfc5
Ryan LeBlanc <ryanleb2003@gmail.com>
2024-01-26 19:08:44 -0400

    Hold to fire/Added comments to code

--- Assets/scripts/Weapon.cs
+++ Assets/scripts/Weapon.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    
     public GameObject BulletPreFab;
     public Transform firePoint;
     public float fireForce = 20f;
 
+    // Shoot method 
+    
     public void Shoot()
     {
+        // Instantiating bullet prefab when shot
+        
         GameObject Bullet = Instantiate(BulletPreFab, firePoint.position, firePoint.rotation);
         Bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.up * fireForce, ForceMode2D.Impulse);
     }
