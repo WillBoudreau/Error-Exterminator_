e3f53ad61b121983b03d079f917398b2097b9171
Ryan LeBlanc <ryanleb2003@gmail.com>
2024-01-26 19:22:33 -0400

    Camera follows player

---
+++ Assets/scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private float damping;
+
+    public Transform target;
+
+    private Vector3 velocity = Vector3.zero;
+
+    private void FixedUpdate()
+    {
+        
+        Vector3 targerPosition = target.position + offset;
+        targerPosition.z = transform.position.z;
+
+        transform.position = Vector3.SmoothDamp(transform.position, targerPosition, ref velocity, damping);
+        
+        
+
+    }
+}
