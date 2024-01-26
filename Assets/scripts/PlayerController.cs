28735e3eff51ffeb12bf9faa2e8e43af5bd3f1cb
Ryan LeBlanc <ryanleb2003@gmail.com>
2024-01-26 18:44:09 -0400

    PlayerController made

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    public Weapon weapon;
+
+    Vector2 moveDirection;
+    Vector2 mousePosition;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movementX = Input.GetAxisRaw("Horizontal");
+        float movementY = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            weapon.Shoot();
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+
+        Vector2 aimDirection = mousePosition - rb.position;
+        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = aimAngle;
+    }
+
+}
