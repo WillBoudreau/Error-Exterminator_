169516d4b2af9a3577cd6cf8902471f4b8098cc6
Ryan LeBlanc <ryanleb2003@gmail.com>
2024-01-27 11:34:58 -0400

    Camera adjusted

--- Assets/scripts/CameraFollow.cs
+++ Assets/scripts/CameraFollow.cs
@@ -6,13 +6,15 @@ public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Vector3 offset;
     [SerializeField] private float damping;
-
+    
     public Transform target;
 
     private Vector3 velocity = Vector3.zero;
 
     private void FixedUpdate()
     {
+        
+        
         
         Vector3 targerPosition = target.position + offset;
         targerPosition.z = transform.position.z;
