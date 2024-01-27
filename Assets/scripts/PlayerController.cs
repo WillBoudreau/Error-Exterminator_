880a720b19ec0f1c3c772746f099b18e628a9f37
WillBoudreau <williamboudreau804@gmail.com>
2024-01-27 14:05:32 -0500

    hp = 0, Game over

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -124,6 +125,11 @@ public class PlayerController : MonoBehaviour
     public void OnCollisionEnter2D(Collision2D collision)
     {
         currentHP--;
+        if(currentHP <= 0)
+        {
+            currentHP = 0;
+            SceneManager.LoadScene(2);
+        }
     }
 
 }
