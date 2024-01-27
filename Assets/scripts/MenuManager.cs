40dcc76949aa38f28978887194a7436dec140800
Koal M. Casler <71401318+KoalMCasler@users.noreply.github.com>
2024-01-26 20:10:23 -0400

    finished Menu Script

--- Assets/scripts/MenuManager.cs
+++ Assets/scripts/MenuManager.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int sceneBuildIndex;
+    public void StartGame()
     {
-        
+        SceneManager.LoadScene(sceneBuildIndex);
     }
-
-    // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
-        
+        //Debug line to test quit function in editor
+        //UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
     }
 }
