df881ef5662f99b39195dfcb05ed0c2ece57b17e
Koal M. Casler <71401318+KoalMCasler@users.noreply.github.com>
2024-01-26 21:39:34 -0400

    Prefabed UI

--- Assets/scripts/UIManager.cs
+++ Assets/scripts/UIManager.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    public static int debugCount;
     // Start is called before the first frame update
     void Start()
     {
-        
+        debugCount = 0;
     }
 
     // Update is called once per frame
