d46701943aaa35a54bf4afb39c1534e5bee813f6
Ryan LeBlanc <ryanleb2003@gmail.com>
2024-01-28 09:42:17 -0400

    PickUp SFX added

--- Assets/scripts/Pickup.cs
+++ Assets/scripts/Pickup.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class Pickup : MonoBehaviour
 {
+    [SerializeField] AudioClip pickupSFX;
+    
     public void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.CompareTag("Player"))
         {
+            AudioSource.PlayClipAtPoint(pickupSFX, transform.position);
             Destroy(this);
         }
     }
+
+    
 }
